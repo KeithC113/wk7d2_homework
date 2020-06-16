502c80d4d2ee9bf792e934b4a9e7d5ba8519e449
KeithC113 <campbell_ka@yahoo.co.uk>
2020-06-16 21:57:56 +0100

    worked on app.js & index.html

--- src/app.js
+++ src/app.js
@@ -6,16 +6,22 @@ document.addEventListener('DOMContentLoaded',()=>{
     el:"#app",
     data: {
       currencies: [],
-      selectedCurrency: null
+      selectedCurrency: null,
+      numEurosAmount: 0
     },
     mounted(){
       this.getCurrencies()
+    },
+    computed:{
+      convertedCurrency: function(){
+        return this.numEurosAmount * this.currencies.rates;
+      }
     },
     methods:{
       getCurrencies: function(){
         fetch("https://api.exchangeratesapi.io/latest")
         .then(response => response.json())
-        .then(currencies => this.currencies = currencies.rates)
+        .then(currencies => this.currencies = currencies.rates);
       }
     }
   })
